7d7b8140237d0bfd90a0dbe573642e875cb9890d
Tejashwini <tejasganne508@gmail.com>
2022-07-19 17:09:00 +0530

    Responsive code changes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function($){
-
+	
 // header code
 $(window).scroll(function() {    
     var scroll = $(window).scrollTop();    
@@ -51,7 +51,13 @@ $(document).ready(function () {
 	
 });
 
-
+AOS.init({
+	offset: 80,
+	duration: 900,
+	delay:100,
+	// once: true,
+	easing: 'ease',
+});
 
 
   });
@@ -80,3 +86,45 @@ $(document).ready(function () {
       }
     }
 });
+
+var span = document.querySelector(".typewriter span");
+var textArr = span.getAttribute("data-text").split(", "); 
+var maxTextIndex = textArr.length; 
+
+var sPerChar = 0.15; 
+var sBetweenWord = 1.5;
+var textIndex = 0; 
+
+typing(textIndex, textArr[textIndex]); 
+
+function typing(textIndex, text) {
+    var charIndex = 0; 
+    var maxCharIndex = text.length - 1; 
+    
+    var typeInterval = setInterval(function () {
+        span.innerHTML += text[charIndex]; 
+        if (charIndex == maxCharIndex) {
+            clearInterval(typeInterval);
+            setTimeout(function() { deleting(textIndex, text) }, sBetweenWord * 1000); 
+            
+        } else {
+            charIndex += 1; 
+        }
+    }, sPerChar * 500); 
+}
+
+function deleting(textIndex, text) {
+    var minCharIndex = 0; 
+    var charIndex = text.length - 1; 
+
+    var typeInterval = setInterval(function () {
+        span.innerHTML = text.substr(0, charIndex); 
+        if (charIndex == minCharIndex) {
+            clearInterval(typeInterval);
+            textIndex + 1 == maxTextIndex ? textIndex = 0 : textIndex += 1; 
+            setTimeout(function() { typing(textIndex, textArr[textIndex]) }, sBetweenWord * 1000); 
+        } else {
+            charIndex -= 1; 
+        }
+    }, sPerChar * 1000); 
+}
